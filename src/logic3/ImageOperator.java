fcc2562650699d2bd8edcf7d0bdb00147534cec8
omargard <44001951+OmarGard@users.noreply.github.com>
2020-03-12 20:36:27 -0600

    -Working al 100. "Bien macizo" como diría el agüelo

--- src/logic3/ImageOperator.java
+++ src/logic3/ImageOperator.java
@@ -28,6 +28,7 @@ class Chunk extends Thread{
 	Lock lock;
 	double coeficient;
 	
+	
 	Chunk(int x, int y, ImageData chunkData1, ImageData chunkData2, String operator, ImageData resultData, Lock lock,double coef){
 		this.operator = operator;
 		cpos.x = x;
@@ -62,7 +63,61 @@ class Chunk extends Thread{
 		int res = (int)Math.floor((a*pa*b*pb)*255/max);
 		return res;	
 	}
-	
+	public void process() {
+		System.out.println ("Thread " + Thread.currentThread().getId());
+	    
+	    int[] lineData1 = new int[width];
+		int[] lineData2 = new int[width];
+		
+		
+	    for (int y = 0; y < height; y++) {
+	    	
+	    	chunkData1.getPixels(0,y,width,lineData1,0);
+	    	chunkData2.getPixels(0,y,width,lineData2,0);
+	       
+	    	for (int x = 0; x<width; x++){
+	            int pixelValue1 = lineData1[x];
+	            int pixelValue2 = lineData2[x];
+	            
+	            argb p1 = new argb(pixelValue1);
+	            argb p2 = new argb(pixelValue2);
+	            
+	            argb pixelResult = new argb();
+	            
+	            Operator<Integer, Integer> op = null;
+	            
+	            // Change behavior depending on the type of operator
+	            switch(operator) {
+	    		case "+":
+	    			op = this::addPixels;
+	    			break;
+	    		case "-":
+	    			op = this::substractPixels;
+	    			break;
+	    		case "*":
+	    			op = this::multiplyPixels;
+	    			break;
+	    		case "#":
+	    			op = this::combinePixels;
+	    			break;
+	    		}
+	            
+	            pixelResult.alpha = op.operate(p1.alpha,p2.alpha);
+	    		pixelResult.red = op.operate(p1.red,p2.red);
+	    		pixelResult.green = op.operate(p1.green,p2.green);
+	    		pixelResult.blue = op.operate(p1.blue,p2.blue);
+	            pixelResult.buildPixelValue();
+	    		
+	            resultData.setPixel(cpos.x+x, cpos.y+y, pixelResult.pixelValue);
+	        }
+	    }
+	    
+	    ImageLoader imageLoader = new ImageLoader();
+	    imageLoader.data = new ImageData[] {resultData};
+	    String workingDir = System.getProperty("user.dir");
+	    
+	    imageLoader.save((workingDir+"/resultados/res" + Thread.currentThread().getId() +".jpg"), SWT.IMAGE_JPEG);
+	}
 	 public void run() { 
 		
 	    System.out.println ("Thread " + Thread.currentThread().getId());
@@ -133,6 +188,7 @@ public class ImageOperator {
 	int chunkRows;
 	int chunkCols;
 	int chunkCounter; //rows x cols
+	int chunksDone;
 	boolean[][] chunkMatrix;
 	double coeficient;
 
@@ -142,6 +198,10 @@ public class ImageOperator {
 	}
 	
 	ImageData getChunkData(ImageData wholeData, int xInicio, int yInicio, int width, int height) {
+		System.out.println("yInicio:"+yInicio+" xInicio:"+xInicio);
+		System.out.println("width:"+width+" height:"+height);
+		System.out.println("yInicio+height:"+(yInicio+height)+" xInicio+width:"+(xInicio+width));
+		
 		ImageData chunkData = new ImageData(width, height, wholeData.depth, wholeData.palette);
 		
 		int i = 0;
@@ -149,6 +209,7 @@ public class ImageOperator {
 		
 		for(int y = yInicio; y < height + yInicio; y++) {
 			for(int x = xInicio; x < width + xInicio; x++) {
+				
 				chunkData.setPixel(i, j, wholeData.getPixel(x, y));
 				i++;
 			}
@@ -170,6 +231,8 @@ public class ImageOperator {
 	public void operate(String operator, int rows, int cols, double coef) throws InterruptedException{
 		chunkMatrix = new boolean[rows][cols];
 		coeficient=coef;
+		int TOTAL_WIDTH=300;
+		int TOTAL_HEIGHT=300;
 		
 		for(int y = 0; y < rows; y++) {
 			for(int x = 0; x < cols; x++) {
@@ -178,16 +241,17 @@ public class ImageOperator {
 		}
 		
 		
-		chunkRows = Math.floorDiv(300, rows);
-		chunkCols = Math.floorDiv(300, cols);
+		chunkRows = Math.floorDiv(TOTAL_HEIGHT, rows);
+		chunkCols = Math.floorDiv(TOTAL_WIDTH, cols);
 		chunkCounter = rows*cols;
-		
+		chunksDone=0;
+		System.out.println("chunkRows:"+chunkRows+" chunkCols:"+chunkCols);
 		//Input Images
 		ImageData dataPic1 = img1.getImageData();
 		ImageData dataPic2 = img2.getImageData();
 		
 		// Resulting Image
-		resultData = new ImageData(300, 300, dataPic1.depth, dataPic1.palette);
+		resultData = new ImageData(TOTAL_WIDTH, TOTAL_HEIGHT, dataPic1.depth, dataPic1.palette);
 		
 		// Chuncks data
 		ImageData chunkData1 = null;
@@ -197,16 +261,31 @@ public class ImageOperator {
 		Lock lock = new Bakery(chunkCounter);
 	
 		// CHUNKS **************************************************
-		for(int y = 0; y < cols*chunkCols; y+=chunkCols) {
-			for(int x = 0; x < rows*chunkRows; x+=chunkRows) {
+		for(int y = 0; y < rows*chunkRows; y+=chunkRows) {
+			for(int x = 0; x < cols*chunkCols; x+=chunkCols) {
 				
-				chunkData1 = getChunkData(dataPic1, x, y, chunkCols, chunkRows);
-				chunkData2 = getChunkData(dataPic2, x, y, chunkCols, chunkRows);
-			
+				int chunkRowstmp=chunkRows;
+				int chunkColstmp=chunkCols;
+				if((y+1)/chunkRows==rows-1) 
+					chunkRowstmp=TOTAL_HEIGHT-y;
+				
+				if((x+1)/chunkCols==cols-1) 
+					chunkColstmp=TOTAL_WIDTH-x;
+				
+				/*System.out.println("x:"+x+" y:"+y);
+				System.out.println("height:"+chunkRowstmp+" width:"+chunkColstmp);
+				System.out.println("x+height:"+(x+chunkRowstmp)+" y+width:"+(y+chunkColstmp));*/
+				
+				chunkData1 = getChunkData(dataPic1, x, y, chunkColstmp, chunkRowstmp);
+				chunkData2 = getChunkData(dataPic2, x, y, chunkColstmp, chunkRowstmp);
+				
 				
 				Chunk chunk = new Chunk(x, y, chunkData1, chunkData2, operator, resultData, lock,coeficient);
+				
 				chunk.start();
 				chunk.join();
+				
+
 			}
 		}
 		
